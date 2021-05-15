9295dd777ad36d0e5dd6f38033267fefae0b9843
saif faisal <seyfff39@gmail.com>
2021-05-15 14:22:16 +0300

    add

---
+++ src/app/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import Login from "../login/Login";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Login />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/login/Login.js
@@ -0,0 +1,61 @@
+import react from "react";
+import classes from "./Login.module.css";
+import logo from "../assets/logo.svg";
+
+export default function Login() {
+  return (
+    <section className={classes.loginPage}>
+      <div className={classes.loginPageContainer}>
+        <div className={classes.loginPageSection}>
+          <div className={classes.loginpageDivider}>
+            <div className={classes.loginPageLogo}>
+              <img src={logo} className={classes.loginPageLogoImage}></img>
+              <h3 className={classes.loginPageLogoText}>
+                fikraCamps dashboard
+              </h3>
+            </div>
+            <div className={classes.form}>
+              <label for="">User Name</label>
+              <input type="text" value="admin" />
+              <label for="">password</label>
+              <input type="password" value="admin123" />
+              <button>Login</button>
+              <input type="checkbox" name="Remember" />
+              <label for="Remember">Remember me</label>
+            </div>
+          </div>
+          <div className={classes.loginpageDivider}>
+            <div className={classes.loginPageCopyright}>
+              {" "}
+              A system by <a href="">saif faisal</a>
+            </div>
+          </div>
+        </div>
+        <div className={classes.loginPageSection}>
+          <h2 className={classes.loginPagepromo}>
+            the favorite dashboard in the ares
+          </h2>
+          <div className={classes.loginpageBackground}></div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+// <div className="main">
+{
+  /* <div className="login-page-logo-div">
+  <img style="width: 40px; height: 40px" src="./images/a.jpg" />
+  <span className="login-page-logo-text">FikraCamps Dashboard</span>
+</div>
+<div className="form">
+  <label for="">User Name</label>
+  <input type="text" value="admin" />
+  <label for="">password</label>
+  <input type="password" value="admin123" />
+  <button>Login</button>
+  <input type="checkbox" name="Remember" />
+  <label for="Remember">Remember me</label>
+</div> */
+}
+// </div>
